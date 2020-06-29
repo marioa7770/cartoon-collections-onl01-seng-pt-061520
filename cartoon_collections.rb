54a7fb9d3410faaf137682581f8daac72020fdbd
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-29 02:44:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves ["Doc", "Dopey", "Bashful",
+  "Grumpy"]
+  puts roll_call_dwarves.each
+end
+
+def summon_captain_planet = ["earth", "wind", "fire", "water", "heart"]# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese cheese_types = ["cheddar", "gouda", "camembert"]
 end
