4a2cc27012dc0eb484da156e7c8ab1793d68c086
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-29 02:43:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,11 @@ def roll_call_dwarves ["Doc", "Dopey", "Bashful",
   puts roll_call_dwarves.each
 end
 
-def summon_captain_planet ["earth", "wind", "fire", "water", "heart"]# code an argument here
+def summon_captain_planet = ["earth", "wind", "fire", "water", "heart"]# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls short_words = ["puff", "go", "two"]
   # Your code here
 end
 
